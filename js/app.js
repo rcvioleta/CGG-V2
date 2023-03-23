414d23f7e2afc426e7916c80564dfc1b6d812670
Rogene Cris Violeta <rcvioleta13@gmail.com>
2023-03-23 15:52:08 +0800

    Game modes and conditional styles.

--- js/app.js
+++ js/app.js
@@ -1,84 +1,109 @@
-const pipe = (...fns) => val => fns.reduce((nv, fn) => fn(nv), val)
+const pipe =
+  (...fns) =>
+  (val) =>
+    fns.reduce((nv, fn) => fn(nv), val);
 
 const randColor = () => {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
   return `${r}, ${g}, ${b}`;
-}
+};
 
 const randColors = function (num) {
-  return new Array(num).fill('').map(() => randColor())
-}
+  return new Array(num).fill("").map(() => randColor());
+};
 
-const randPickColor = colorArr => colorArr[Math.floor(Math.random() * colorArr.length)];
+const randPickColor = (colorArr) =>
+  colorArr[Math.floor(Math.random() * colorArr.length)];
 
 const gameModeObj = {
   easy: 4,
   medium: 6,
   hard: 8,
 };
-const DEFAULT_GAME_MODE = gameModeObj.medium;
+const DEFAULT_GAME_MODE = gameModeObj.easy;
 let gameMode = DEFAULT_GAME_MODE;
 
-const colorsArray = randColors(gameMode);
-const SELECTED_COLOR = randPickColor(colorsArray);
+let colorsArray = randColors(gameMode);
+let selectedColor = randPickColor(colorsArray);
 
-console.log(colorsArray)
-console.log(SELECTED_COLOR)
+const delChildBoxes = () => {
+  const boxes = document.querySelectorAll(".box-wrapper > .box");
+  boxes.forEach((box) => box.parentNode.removeChild(box));
+};
 
-// document.getElementById('display-color').textContent = SELECTED_COLOR;
+const setGameModes = () => {
+  const gameModesEl = document.querySelectorAll(".modes > ul > li");
 
-// const gameModesEl = document.querySelectorAll('.modes > ul > li');
+  for (let i = 0; i < gameModesEl.length; i++) {
+    gameModesEl[i].addEventListener("click", function (evt) {
+      const el = evt.target;
+      for (let menuItem of el.parentNode.children) {
+        menuItem.classList.remove("active");
+      }
+      el.classList.add("active");
+      const userSelectedMode = el.textContent.trim().toLowerCase();
 
-// for (let i = 0; i < gameModesEl.length; i++) {
-//   gameModesEl[i].addEventListener('click', function (evt) {
-//     const userSelectedMode = evt.target.textContent.trim().toLowerCase();
-
-//     if (!Object.keys(gameModeObj).includes(userSelectedMode)) {
-//       return;
-//     }
-
-//     gameMode = gameModeObj[userSelectedMode];
-//   });
-// }
-
-const boxes = document.querySelectorAll('.box-wrapper > .box');
-
-const boxClickHandler = evt => {
-  const el = evt.target;
-  const userSelectedColor = el.style.backgroundColor.match(/(\d+)([\d\s\,]+)/gim)[0];
-  if (userSelectedColor === SELECTED_COLOR) {
-    document.querySelector('header').setAttribute('style', `background-color: rgb(${SELECTED_COLOR});`);
-    document.querySelectorAll('.box-wrapper > .box').forEach(box => {
-      box.setAttribute('style', `background-color: rgb(${SELECTED_COLOR});`)
-      box.textContent = '';
+      if (!Object.keys(gameModeObj).includes(userSelectedMode)) {
+        return;
+      }
+      gameMode = gameModeObj[userSelectedMode];
+      colorsArray = randColors(gameMode);
+      selectedColor = randPickColor(colorsArray);
+      delChildBoxes();
+      init();
     });
-    return alert("Congrats, you win!");
+  }
+};
+
+const boxClickHandler = (evt) => {
+  const el = evt.target;
+  const userSelectedColor =
+    el.style.backgroundColor.match(/(\d+)([\d\s\,]+)/gim)[0];
+  const boxes = document.querySelectorAll(".box-wrapper > .box");
+
+  if (userSelectedColor === selectedColor) {
+    document
+      .querySelector("header")
+      .setAttribute("style", `background-color: rgb(${selectedColor});`);
+    boxes.forEach((box) => {
+      box.setAttribute("style", `background-color: rgb(${selectedColor});`);
+      box.textContent = "";
+    });
+    document.getElementById("controls").style.backgroundColor = "green";
+    document.querySelector("#controls > .message").textContent =
+      "Congrats, you win!";
+    return cleanupEventListeners();
   }
   el.textContent = "X";
-  el.setAttribute('style', 'background-color: red;');
-}
+  el.setAttribute("style", "background-color: red;");
+};
+
+const buildBoxes = () => {
+  const boxWrapper = document.querySelector(".box-wrapper");
+  for (let i = 0; i < gameMode; i++) {
+    const div = document.createElement("div");
+    div.classList.add("box");
+    div.setAttribute("style", `background-color: rgb(${colorsArray[i]});`);
+    div.addEventListener("click", boxClickHandler, false);
+    boxWrapper.appendChild(div);
+  }
+};
+
+const cleanupEventListeners = () => {
+  // remove event listeners here
+  const boxes = document.querySelectorAll(".box-wrapper > .box");
+  boxes.forEach((box) => box.removeEventListener("click", boxClickHandler));
+};
 
 const init = () => {
   // display the boxes according to the game mode
-  document.getElementById('display-color').textContent = SELECTED_COLOR;
-
-  const boxWrapper = document.querySelector('.box-wrapper')
-  for (let i = 0; i < gameMode; i++) {
-    const div = document.createElement('div');
-    div.classList.add('box');
-    div.setAttribute('style', `background-color: rgb(${colorsArray[i]});`)
-    div.addEventListener('click', boxClickHandler, false);
-    boxWrapper.appendChild(div);
-  };
-}
+  document.getElementById("display-color").textContent = selectedColor;
+  buildBoxes();
+  setGameModes();
+};
 
 init();
 
-window.onbeforeunload = () => {
-  // remove event listeners here
-  for (let i = 0; i < boxes.length; i++) {
-    boxes[i].removeEventListener('click', boxClickHandler);
-  };
-}
+window.onbeforeunload = cleanupEventListeners;
